fb4a18aab73e4bbc8876ea9be357eaf31a110a5b
VityaMuzychka <mirrorvitya@gmail.com>
2023-11-19 23:05:02 +0200

    a bit edited

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App() {
 
   return (
     <div className="App">
-     <Person name='Anna' email='anna@gmail.com' age={21} isMarried={true} friends={['Inna', 'Olena', 'Vika']} region={Region.Podillya} />
+     <Person name='Anna' email='anna@gmail.com' age={21} isMarried={true} friends={['Inna', 'Olena', 'Vika']}
+      region={Region.Podillya} />
     </div>
   );
 }
